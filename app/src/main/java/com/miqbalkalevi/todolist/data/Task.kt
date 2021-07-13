cc12c5330867f67e39d7cb089dbca0107c95dcd0
iqbalklv <49069024+iqbalklv@users.noreply.github.com>
2021-07-14 02:16:00 +0700

    Add layouts, Task Model

---
+++ app/src/main/java/com/miqbalkalevi/todolist/data/Task.kt
@@ -0,0 +1,21 @@
+package com.miqbalkalevi.todolist.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "table_task")
+@Parcelize
+data class Task(
+    val name: String,
+    val isImportant: Boolean = false,
+    val isDone: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+
+): Parcelable {
+    val createdFormatted: String
+        get() = DateFormat.getDateInstance().format(created)
+}
