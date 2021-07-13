f0e12dcca059f25282631a54c9d8349dc34ac493
iqbalklv <49069024+iqbalklv@users.noreply.github.com>
2021-07-14 02:45:12 +0700

    Add navigation

--- app/src/main/java/com/miqbalkalevi/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/miqbalkalevi/todolist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.miqbalkalevi.todolist
+package com.miqbalkalevi.todolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miqbalkalevi.todolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/miqbalkalevi/todolist/ui/tasks/TasksFragment.kt
@@ -0,0 +1,6 @@
+package com.miqbalkalevi.todolist.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.miqbalkalevi.todolist.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks)
